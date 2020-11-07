65064c118e21c88fcf4e033b3fcb14e7d09848bc
Satvik Borra <borrasatvik@gmail.com>
2020-11-07 17:56:42 -0500

    file upload and getting barcode id

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,25 +3,53 @@ var request = require("request");
 const fs = require("fs");
 var path = require("path");
 const { randomInt } = require("crypto");
+var mime = require('mime-types')
 var router = express.Router();
 
+resolve = path.resolve;
 
-router.get("/sendbarcode/:imgBase64", function (req, res, next) {
-    // res.send(req.params);
-    // res.send(user1.id);
+var CloudmersiveBarcodeapiClient = require('cloudmersive-barcodeapi-client');
+var defaultClient = CloudmersiveBarcodeapiClient.ApiClient.instance;
 
-    // send img to barcode api
+var Apikey = defaultClient.authentications['Apikey'];
+Apikey.apiKey = "bee7aebf-526b-4839-b650-f687c86f69bf"
+var apiInstance = new CloudmersiveBarcodeapiClient.BarcodeScanApi();
 
-    // get back UPC
 
-    // send UPC to food API, get back food data
+router.post("/sendbarcode", function (req, res, next) {
+  // send img to barcode api
+  var imageFile = req.files.image.tempFilePath;
+  var fileExtension = mime.extension(req.files.image.mimetype) 
 
-    // parse api data
+  var fullName = imageFile+"."+fileExtension;
+  fs.renameSync(imageFile, fullName);
 
-    //send back food data
-    res.json(
-      { "test":"hi_"+req.params.imgBase64 }
-    );
+  var callback = function(error, data, response) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('API called successfully. Returned data: ' + data);
+
+      // get back UPC
+      var barcodeType = data["BarcodeType"]
+      var barcodeID = data["RawText"]
+
+      // send UPC to food API, get back food data
+
+
+      // parse api data
+
+      //send back food data
+      res.json(
+        { "test":data }
+      );
+    }
+
+
+  };
+  
+  var finalImage = Buffer.from(fs.readFileSync(fullName).buffer);
+  apiInstance.barcodeScanImage(finalImage, callback);
 });
 
 // router.post("/checkout", function (req, res, next) {
